b0627e3d5ccbd705fd711ab1595d3fb279c4430c
Matt Bechtel <bechma02@gmail.com>
2019-10-28 03:54:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ def map(source_array)
   return new_array
 end
 
-def reduce(source_array,starting_value=0)
+def reduce(source_array,starting_value=true)
   store_value = starting_value
   i = 0
   while i < source_array.length
